138b10e31f70fee751f4de01d30d52d5eefa8c68
Marc Karimi <99436236+certifiedp@users.noreply.github.com>
2024-07-18 16:07:48 +0000

    lol

--- src/example_package_certifiedp/_version.py
+++ src/example_package_certifiedp/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev11+gde9bd24'
-__version_tuple__ = version_tuple = (0, 1, 'dev11', 'gde9bd24')
+__version__ = version = '0.1.dev12+g71c78ab.d20240718'
+__version_tuple__ = version_tuple = (0, 1, 'dev12', 'g71c78ab.d20240718')
